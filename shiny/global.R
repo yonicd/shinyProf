45d567e8adfc394a5007cd37457ca18e1a86d859
yonicd <yonicd@gmail.com>
2017-01-24 09:48:51 -0500

    update global.R

--- shiny/global.R
+++ shiny/global.R
@@ -4,14 +4,5 @@ library(reshape2)
 library(dplyr)
 library(RColorBrewer)
 
-# GS2net=function(df,low,high,ctxf){
-#   df.mat=df[df$timef>=low&df$timef<=high,]
-#   if(ctxf!='') df.mat=df.mat[df.mat$ctx>=ctxf,]
-#   df.mat=df.mat%>%dcast(ctx+label~dependsOn,value.var='o')
-#   browser()
-#   row.names(df.mat)=df.mat[,2]
-#   df.mat[,-c(1,2,8)]%>%as.matrix
-# }
-
 load('../GS.rda')
 GS.net$timeid=1:nrow(GS.net)
